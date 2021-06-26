73cc6f82f05bcb052a988453ae1440af462d7d21
ollily <coding@glowa-net.com>
2021-06-26 16:06:11 +0200

    gm-scripting-1-[Minor Updates 2021 #1]-update
    Signed-off-by: ollily <coding@glowa-net.com>

--- grab_links/build_env.cmd
+++ grab_links/build_env.cmd
@@ -3,9 +3,9 @@ set SRC_DIR=%~dp0\tpl
 set TARGET_NAME=%MODULE_NAME%.user.js
 
 set DEPLOY_FOLDER=%MODULE_NAME%
-set DEPLOY_NAME=82191.user.js
+rem set DEPLOY_NAME=82191.user.js
 
-set C1=%COMMON_DIR%\gm_base.user.js.RAW.js
+rem set C1=%COMMON_DIR%\gm_base.user.js.RAW.js
 set C2=%TOOLS_DIR%\gm4-polyfill.user.js.RAW.js
 
 set P1=%FILE_PREFIX%%MODULE_NAME%.js
--- grab_links/tpl/tpl_head1.js
+++ grab_links/tpl/tpl_head1.js
@@ -8,14 +8,14 @@
 // @author          ollily2907
 // @license         Apache License, Version 2.0
 // @license         https://www.apache.org/licenses/LICENSE-2.0.txt
-// @homepageURL     https://github.com/ollily/gm-scripting
-// @supportURL      https://github.com/ollily/gm-scripting
-// @downloadURL     https://raw.githubusercontent.com/ollily/gm-scripting/grab_links/grab_links.user.js
-// @installURL      https://raw.githubusercontent.com/ollily/gm-scripting/grab_links/grab_links.user.js
-// @updateURL       https://raw.githubusercontent.com/ollily/gm-scripting/grab_links/grab_links.user.js
-// @source          https://raw.githubusercontent.com/ollily/gm-scripting/grab_links/grab_links.user.js
+// @homepageURL     http://ollily.github.io/gm-scripting/
+// @supportURL      http://ollily.github.io/gm-scripting/
+// @downloadURL     https://raw.githubusercontent.com/ollily/gm-scripting/master/grab_links/grab_links.user.js
+// @installURL      https://raw.githubusercontent.com/ollily/gm-scripting/master/grab_links/grab_links.user.js
+// @updateURL       https://raw.githubusercontent.com/ollily/gm-scripting/master/grab_links/grab_links.user.js
+// @source          https://raw.githubusercontent.com/ollily/gm-scripting/master/grab_links/grab_links.user.js
 // @icon            https://raw.githubusercontent.com/ollily/gm-scripting/master/grab_links/resource/gl_logo.png
 // @compatible      chrome
 // @compatible      firefox
-// @namespace       http://userscripts.org/users/ollily
+// @namespace       https://github.com/ollily
 // @run-at          document-end
